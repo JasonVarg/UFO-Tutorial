2711be9b3b30bc649a9b490ce0c0abc4838d2dfb
Jason Vargas <javarg410@knights.ucf .edu>
2019-07-11 16:52:16 -0400

    Added functionality to the rest of the game

--- UFO Game/Assets/Scripts/PlayerController.cs
+++ UFO Game/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-public float speed;
+    public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody2D rb2d;
+    private int count;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D> ();
+
+        count = 0;
+        winText.text = "";
+        
+        SetAllText();
     }
     void FixedUpdate()
     {
@@ -20,5 +29,27 @@ public float speed;
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         
         rb2d.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))Application.Quit();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+
+            SetAllText();
+        }
+
+        if(count == 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    void SetAllText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
